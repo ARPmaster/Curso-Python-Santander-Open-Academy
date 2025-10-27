af141b4a89a4a5d88178e214c4021ac15ea817c0
thekingofwar03 <adrianforgithub@gmail.com>
2025-10-27 17:11:48 +0100

    Aprendiendo python curso Santander Open  Academy

---
+++ prueba.py
@@ -0,0 +1,44 @@
+"""
+Estructura de if else
+
+if condicion:
+accion
+else:
+accion
+#comentario de una linea
+"""
+
+"""
+
+comentario multilinea
+
+si se desea  concatenar operaciones  
+accion1;accion2;accion3
+"""
+edad=25 #ejemplo de int
+precio=9.99 #ejemplo de float
+cadena="ejemplo" #ejemplo de string
+bolean =True #ejemplo con boolean
+#para insertar comillas dentro de una cadena es \"" por ejemplo
+es_estudiante=True
+division=10//3 #Esto descarta  la parte decimal a diferencia del /
+exponente=2**2 #Esto sirve para elevar
+
+#Operadores de comparación
+a=1
+b=2
+
+igual = a==b #estod devuelve directamente  un valor booleano 
+dif=a!=b #esto devuelve un booleano
+mayor=a>b
+menor=a<b
+mayorigual=a>=b
+menorigual=a<=b
+#son operadores basicos suelen usarse para obtener valores boleanos
+#Operadores logicos
+resultado_and=(a!=0) and(b!=1)
+resultado_or=(a!=0) or(b!=1)
+resultado_not= not(a==1)
+
+
+
